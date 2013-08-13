be4091d4d4c8e4385b2acc43665eab2f8a746894
Craig Sharkie <craig.sharkie@gmail.com>
2013-08-13 14:25:15 +1000

    ADDED jQuery UI
    To access droppable method, added jQuery UI

--- scripts/setup.js
+++ scripts/setup.js
@@ -7,6 +7,10 @@ require.config({
       "//code.jquery.com/jquery-1.10.2.min",
       "/_jquery/js/1.9.1.min"
     ],
+    "jQuery UI": [
+      "//code.jquery.com/ui/1.10.3/jquery-ui",
+      "/_jquery/jqueryui/js/jquery-ui-1.10.2.custom.min"
+    ],
     "Handlebars": [
       "//cdnjs.cloudflare.com/ajax/libs/handlebars.js/1.0.0/handlebars.min",
       "/_handlebars/js/1.0.0.min"
@@ -22,7 +26,7 @@ require.config({
   waitSeconds: 15
 });
 
-require( ["uuid", "jQuery", "Handlebars", "events", "scripts"],
+require( ["uuid", "jQuery", "jQuery UI", "Handlebars", "events", "scripts"],
   function() {
     window.uuid = arguments[0];//skirts around an implementation issue with the library
 
